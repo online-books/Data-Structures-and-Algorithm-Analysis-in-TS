e1d2bf705c4e3dd8bf1ef076db5a823eb5fb376d
fariyAhill <zuoanhuating@163.com>
2020-07-02 11:17:10 +0800

    stack

--- src/data-structures/stack/test.ts
+++ src/data-structures/stack/test.ts
@@ -6,19 +6,16 @@ import {
 
 describe("栈", () => {
   test("平衡符号", () => {
-    const str = "[a*(b+c)+d*(e+f)]";
-    const result = balanceSymbol(str);
-    expect(result).toBeTruthy();
+    expect(balanceSymbol("[a*(b+c)+d*(e+f)]")).toBeTruthy();
   });
   test("计算后缀表达式", () => {
-    const expression = "123*+45*+6-";
-    const result = postfixExpression(expression);
-    expect(result).toBe(21);
+    expect(postfixExpression("123*+45*+6-")).toBe(21);
   });
   test("中缀转后缀表达式", () => {
-    const expression = "1+2*3+(4+5*6+7+8)*9";
-    const result = transformInFixExpressionToPostfixExpression(expression);
-    expect(result).toBe("123*+456*+78++9*+");
-    expect(postfixExpression(result)).toBe(448);
+    const expression = transformInFixExpressionToPostfixExpression(
+      "1+2*3+(4+5*6+7+8)*9"
+    );
+    expect(expression).toBe("123*+456*+78++9*+");
+    expect(postfixExpression(expression)).toBe(448);
   });
 });
