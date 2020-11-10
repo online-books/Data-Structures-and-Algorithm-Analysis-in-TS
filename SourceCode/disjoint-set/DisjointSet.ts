77e216793ad65e8e1fa6eb4a0e6ab0c14e1ccb1d
qindagang <zuoanhuating@163.com>
2020-11-10 18:04:52 +0800

    Disjoint Set

--- SourceCode/disjoint-set/DisjointSet.ts
+++ SourceCode/disjoint-set/DisjointSet.ts
@@ -1,38 +1,42 @@
 /** @format */
 
 export default class DisjointSet {
-    private data: number[]
-    constructor(len: number) {
-        this.data = new Array(len).fill(-1)
+    private list: number[]
+    constructor(size: number) {
+        this.list = new Array(size).fill(-1)
     }
     public find(index: number): number {
-        const {data} = this
         this.checkRange(index)
-        const parentIndex = data[index]
+        const {list} = this
+        const parentIndex = list[index]
         if (parentIndex > 0) {
             const rootIndex = this.find(parentIndex)
-            data[index] = rootIndex
+            list[parentIndex] = rootIndex
             return rootIndex
         }
-        return index
+        return parentIndex
     }
-    public union(rootIndex1: number, rootIndex2: number): number {
-        const {data} = this
+    public union(rootIndex1: number, rootIndex2: number): void {
         this.checkRange(rootIndex1)
         this.checkRange(rootIndex2)
-        const treeSize1 = data[rootIndex1]
-        const treeSize2 = data[rootIndex2]
+        const {list} = this
+        const treeSize1 = list[rootIndex1]
+        const treeSize2 = list[rootIndex2]
+        if (treeSize1 >= 0) {
+            return this.union(treeSize1, rootIndex2)
+        }
+        if (treeSize2 >= 0) {
+            return this.union(rootIndex1, treeSize2)
+        }
         if (treeSize1 > treeSize2) {
             return this.union(rootIndex2, rootIndex1)
         }
-        data[rootIndex1] = treeSize1 - 1
-        data[rootIndex2] = rootIndex1
-        return treeSize1 - 1
+        list[rootIndex1] = treeSize1 - 1
+        list[rootIndex2] = rootIndex1
     }
     private checkRange(index: number) {
-        const {data} = this
-        if (index >= data.length || index < 0) {
-            throw new RangeError('out of range')
+        if (index < 0 || index >= this.list.length) {
+            throw Error('index is invalid')
         }
     }
 }
