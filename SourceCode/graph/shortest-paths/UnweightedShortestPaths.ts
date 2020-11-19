2de9d26cb44f1eecd75546ee3d6b28968a68e5a4
qindagang <zuoanhuating@163.com>
2020-11-19 10:24:23 +0800

    Graph

---
+++ SourceCode/graph/shortest-paths/UnweightedShortestPaths.ts
@@ -0,0 +1,37 @@
+/** @format */
+
+import Queue from '@/queue/Queue'
+import DirectedGraph from '../DirectedGraph'
+
+interface ShortestPathsMap {
+    [propName: string]: string[]
+}
+
+export default function unWeightedShortestPaths(directedGraph: DirectedGraph, vertexName: string): ShortestPathsMap {
+    const queue = new Queue<string>()
+    const result: ShortestPathsMap = {}
+    const visitedVertices = directedGraph.getAllVertices()
+    let counter = 0
+    queue.enqueue(vertexName)
+    result[vertexName] = [vertexName]
+    while (queue.size) {
+        const curVertexName = queue.dequeue()!
+        directedGraph.traverseAdjVertices(curVertexName, (adjVertexName, adjVertexIndex) => {
+            if (!result[adjVertexName]) {
+                result[adjVertexName] = [...result[curVertexName]].concat(adjVertexName)
+                visitedVertices[adjVertexIndex] = ''
+                queue.enqueue(adjVertexName)
+                counter += 1
+            }
+        })
+    }
+    if (counter) {
+        visitedVertices.forEach(visitedVertexName => {
+            if (visitedVertexName) {
+                result[visitedVertexName] = []
+            }
+        })
+    }
+    delete result[vertexName]
+    return result
+}
